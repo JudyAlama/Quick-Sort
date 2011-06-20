a41aa786a496703ab7df79c74f457e3073544a73
Ippei KIno <ippeikino@gmail.com>
2011-06-21 00:24:04 +0900

    fix algorism (but error occurred)

--- src/main.c
+++ src/main.c
@@ -1,58 +1,125 @@
 #include <stdio.h>
 
-int quicksort (int numsc, int numsv[]) {
+int DEBUG = 1;
+
+/*
+ * return higher
+ */
+int
+pivot_picker(int num1, int num2, int num[])
+{
+    return (num[num1] <= num[num2] ? num1 : num2);
+}
+
+void
+print_nums(int begin, int end, int nums[])
+{
     int i;
-    int l = 0, r = numsc -1;
-    int lastl, lastr;
-    int tmp;
-    int pivot = (numsv[0] <= numsv[1] ? numsv[0] : numsv[1]);
-    int solve[numsc];
-    
-    for (i = 0; i < numsc; i++) {
-        printf("%d, ", numsv[i]);
+
+    for (i = begin; i < end - 1; i++) {
+        printf("%d, ", nums[i]);
     }
-    printf("\n");
+    printf("%d\n", nums[end]);
+}
+
+int
+quicksort(int head, int tail, int num[])
+{
+    int l = head, r = tail;
+    int l_hit, r_hit;
+    int tmp;
+    int pivot = pivot_picker(head, head + 1, num);
+    int rval;
+    
+    if (DEBUG != 0)
+        print_nums(head, tail, num);
+
+    // swap pivot
+    tmp = num[pivot];
+    num[pivot] = num[head];
+    num[head] = tmp;
+    l++;
 
     while (l < r) {
-        for (l = l; l < numsc; l++) {
-            if (numsv[l] >= pivot) {
-                printf("%d(l = %d)\n", numsv[l], l);
+        l_hit = 0;
+        r_hit = 0;
+        for (l = l; l < tail + 1; l++) {
+            if (num[l] >= num[head]) {
+                l_hit = 1;
+                if (DEBUG != 0)
+                    printf("%d(l = %d)\n", num[l], l);
+                break;
+            } else if (l == r) {
                 break;
             }
         }
-        for (r = r; r > 0; r--) {
-            if (numsv[r] < pivot) {
-                printf("%d(r = %d)\n", numsv[r], r);
+        for (r = r; r > head - 1; r--) {
+            if (num[r] < num[head]) {
+                r_hit = 1;
+                if (DEBUG != 0)
+                    printf("%d(r = %d)\n", num[r], r);
+                break;
+            } else if (l == r) {
                 break;
             }
         }
-
-        tmp = numsv[r];
-        numsv[r] = numsv[l];
-        numsv[l] = tmp;
+        if (l_hit == 1 && r_hit == 1) {
+            tmp = num[r];
+            num[r] = num[l];
+            num[l] = tmp;
+        } else if (l_hit == 1 && r_hit == 0) {
+            tmp = num[head];
+            num[head] = num[l];
+            num[l] = tmp;
+            l--;
+            r--;
+            break;
+        } else if (l_hit == 0 && r_hit == 1) {
+            tmp = num[head];
+            num[head] = num[r];
+            num[r] = tmp;
+            l--;
+            r--;
+            break;
+        }
 
         l++;
         r--;
     }
 
-    printf("pivot = %d, l = %d, r = %d\n", pivot, l, r);
-    for (i = 0; i < numsc; i++) {
-        printf("%d, ", numsv[i]);
+    if (DEBUG != 0) {
+        printf("pivot = %d, l = %d, r = %d\n", num[head], l, r);
+        print_nums(head, tail, num);
     }
-    printf("\n");
+
+    //quicksort();
+    if (l == head)
+        rval = quicksort(head, l, num);
+    if (r == tail)
+        rval = quicksort(r, tail, num);
 
     return 0;
 }
 
-
-int main (int argc, char** argv) {
-    int nums[] = {49, 10, 6, 30, 50, 2, 17};
+int
+main(int argc, char* argv[])
+{
+    int n_nums1, n_nums2;
+    int nums1[] = {49, 10, 6, 30, 50, 2, 17};
     int nums2[] = {8, 49, 10, 6, 30, 50, 2, 17};
     int rval;
 
-    rval = quicksort(sizeof nums / sizeof nums[0], nums);
-    rval = quicksort(sizeof nums / sizeof nums2[0], nums2);
+    n_nums1 = sizeof nums1 / sizeof nums1[0];
+    n_nums2 = sizeof nums2 / sizeof nums2[0];
 
-    return rval;
+    print_nums(0, n_nums1 - 1, nums1);
+    print_nums(0, n_nums2 - 1, nums2);
 
+    rval = quicksort(0, n_nums1 - 1, nums1);
+    rval = quicksort(0, n_nums2 - 1, nums2);
+
+    print_nums(0, n_nums1 - 1, nums1);
+    print_nums(0, n_nums2 - 1, nums2);
+
+    return 0;
 }
